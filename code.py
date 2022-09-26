c18d19a91b4dbd722a6fe04fae76dab1351c953f
Janusz Dziurzynski <janusz@corechain.tech>
2022-09-26 06:41:28 -0400

    WIP

--- code.py
+++ code.py
@@ -1,1 +1,28 @@
-print("Hello World!")
+print("Clickity-clack!")
+
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners import DiodeOrientation
+
+keyboard = KMKKeyboard()
+
+keyboard.col_pins = (board.D6, board.D7, board.D8, board.D9, board.D10)
+keyboard.row_pins = (board.D0, board.D1, board.D2, board.D3, board.D4, board.D5)
+
+keyboard.diode_orientation = DiodeOrientation.COLUMNS
+
+keyboard.keymap = [
+    [KC.Q, KC.Q, KC.Q, KC.Q, KC.Q],
+    [KC.Q, KC.Q, KC.Q, KC.Q, KC.Q],
+    [KC.Q, KC.Q, KC.Q, KC.Q, KC.Q],
+    [KC.Q, KC.Q, KC.Q, KC.Q, KC.Q],
+    [KC.Q, KC.Q, KC.Q, KC.Q, KC.Q],
+    [KC.Q, KC.Q, KC.Q, KC.Q, KC.Q],
+]
+
+keyboard.debug_enabled = True
+
+if __name__ == '__main__':
+    keyboard.go()
